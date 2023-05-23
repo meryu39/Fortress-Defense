dbd9e64bc1c263393d6d006fc0da5347a89b3ec4
meryu39 <62229467+meryu39@users.noreply.github.com>
2023-05-23 22:02:39 +0900

    아군, 적 유닛 생성
    동시 출력하고 충돌 구현남음
    그리고 생성된 유닛에 대해서 배열로 선언해야할듯

--- [DataStructure]Team Project/[DataStructure]Team Project/main.cpp
+++ [DataStructure]Team Project/[DataStructure]Team Project/main.cpp
@@ -4,13 +4,61 @@
 #include"Resource.h"
 
 
+void sleep_time(int milliseconds) {
+    clock_t startTime = clock();
+    while ((clock() - startTime) * 1000 / CLOCKS_PER_SEC < milliseconds) {
+        //기다리기
+    }
+}
+
+void my_unit(FieldData testData[FIELD_HEIGHT][FIELD_WIDTH], int testX)
+{
+    for (int y = 0; y < FIELD_HEIGHT; y++)
+    {
+        for (int x = 0; x < FIELD_WIDTH; x++)
+        {
+            testData[y][x].code = 0;
+            if (x == testX && y == FIELD_HEIGHT - 1)
+            {
+                testData[y][x].code = 1;
+            }
+            testData[y][x].shape.look = 'M';
+            testData[y][x].shape.color = 7;
+        }
+    }
+}
+
+void enemy_unit(FieldData testData[FIELD_HEIGHT][FIELD_WIDTH], int testX)
+{
+    for (int y = 0; y < FIELD_HEIGHT; y++)
+    {
+        for (int x = 0; x < FIELD_WIDTH; x++)
+        {
+            testData[y][x].code = 0;
+            // 정반대편 x 좌표에서도 유닛 생성
+            if (x == FIELD_WIDTH - testX && y == FIELD_HEIGHT - 1)
+            {
+                testData[y][x].code = 1;
+
+            }
+            testData[y][x].shape.look = 'E';
+            testData[y][x].shape.color = 7;
+
+        }
+    }
+}
+
 int main(void)
 {
 	FieldData testData[FIELD_HEIGHT][FIELD_WIDTH];
     char test[FIELD_WIDTH + 2];
 	int temp = 0;
     bool sign = FALSE;
-    int testX = 5;
+    int testX1 = 5; // 아군 유닛의 testX 값
+    int testX2 = 5; // 적 유닛의 testX 값
+    int inputKey = 0;  // 입력된 키를 저장할 변수
+
+
 
     printScreen(NULL);
     for (int y = 0; y < FIELD_HEIGHT; y++)
@@ -22,27 +70,28 @@ int main(void)
             testData[y][x].shape.color = 7;
         }
     }
+
+
     while (temp != -1)
     {
-        for (int y = 0; y < FIELD_HEIGHT; y++)
-        {
-            for (int x = 0; x < FIELD_WIDTH; x++)
-            {
-                testData[y][x].code = 0;
-                if (x == testX && y == FIELD_HEIGHT - 1)
-                {
-                    testData[y][x].code = 1;
-                }
-                testData[y][x].shape.look = 'A';
-                testData[y][x].shape.color = 7;
-            }
-            
+        if (kbhit()) {
+            inputKey = getch();
         }
-        testX += 1;
+
+        if (inputKey == 'q') {
+            my_unit(testData, testX1); // 아군 유닛 생성
+            testX1 += 1; // 아군 유닛 x좌표 증가
+        }
+        else if (inputKey == 'w') {
+            enemy_unit(testData, testX2); // 적 유닛 생성
+            testX2 += 1; // 적 유닛 x좌표 증가
+        }
+
+
         goto_xy(FIELD_WIDTH, FIELD_HEIGHT);
-        _sleep(100);
+        sleep_time(50);
         printScreen(testData);
-        if (testX > FIELD_WIDTH)  break;
+        if (testX1 > FIELD_WIDTH && testX2 > FIELD_WIDTH) break;
         
     }
     
